42ce65ff79177db5234d78f927a4972afc776839
Reid <touyalin@gmail.com>
2022-10-13 23:33:16 +0800

    Created pages controller and related html page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@
 
 Rails.application.routes.draw do
   resources :portfolios
+
+  get "about-me", to: "pages#about"
+  get "contact", to: "pages#contact"
+
+  root to: "pages#home"
 end
