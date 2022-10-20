7c7bdb1b3715a09ba0b1c102373c59774ec7bf68
Reid <touyalin@gmail.com>
2022-10-20 23:01:04 +0800

    add blog toggle status route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    member do
+      get :toggle_status
+    end
+  end
 
   resources :portfolios, except: [:show]
   get "portfolio/:id", to: "portfolios#show", as: "portfolio_show"
