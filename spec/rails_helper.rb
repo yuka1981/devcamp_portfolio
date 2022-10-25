852613c47bfc5d121bfce4192b4aa7c75f2f3a3c
Reid <touyalin@gmail.com>
2022-10-25 23:38:34 +0800

    install and setup shoulda-mathcer gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,10 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+
 ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
@@ -29,6 +33,13 @@ begin
 rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
 
 # Capybara.register_driver :selenium_chrome do |app|
