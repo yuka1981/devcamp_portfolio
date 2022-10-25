42a1eddc699632f8f38c49e99486e4795e882979
Reid <touyalin@gmail.com>
2022-10-25 23:58:21 +0800

    add set default value to portfolio model and fix association rspec

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -2,4 +2,13 @@
 
 class Portfolio < ApplicationRecord
   validates :title, :body, :main_image, :thumb_image, presence: true
+
+  after_initialize :set_default
+
+  private
+
+  def set_default
+    self.main_image ||= "https://via.placeholder.com/600x400"
+    self.thumb_image ||= "https://via.placeholder.com/350x200"
+  end
 end
